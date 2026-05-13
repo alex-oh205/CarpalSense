d78a46b99d1fe11947ba402148f3f64c6f5ae1cb
Shiv-Patel27 <shivranger765@gmail.com>
2026-05-13 12:28:20 -0400

    changed master code

--- bme_materials/oymotion_emg/oymotion_emg.ino
+++ bme_materials/oymotion_emg/oymotion_emg.ino
@@ -43,9 +43,9 @@ const unsigned long timeBudget = 2000;
 // Percent spread = ((max - min) / min) * 100
 
 const unsigned long WINDOW_MS       = 1000;
-const float         THRESH_LOW_PCT  = 25.0f;
-const float         THRESH_MED_PCT  = 50.0f;
-const float         THRESH_HIGH_PCT = 75.0f;
+const float         THRESH_LOW_PCT  = 1000.0f;
+const float         THRESH_MED_PCT  = 3000.0f;
+const float         THRESH_HIGH_PCT = 6000.0f;
 
 unsigned long windowStart = 0;
 long          windowMin   = LONG_MAX;
